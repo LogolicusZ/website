3bd0011727492af77c625b43a4845129025cd5fd
Sangelo <contact@sangelo.space>
2024-11-26 20:57:31 +0100

    [a] initial gallery

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import "$styles/gallery/index.scss";
+
+  const images = import.meta.glob("/static/assets/img/gallery/*");
+  const imageAlts: ImageAlts = {
+    "pic01.avif": "A beautiful sunset",
+    "pic02.avif": "Mountain landscape",
+    "pic03.avif": "City skyline at night",
+    "pic28.avif": "Picture 28",
+  };
+  const getAltText = (filename: string | undefined) => {
+    if (!filename) return "No alt text set.";
+    return imageAlts[filename] || `Image ${filename}`;
+  };
+
+  interface ImageAlts {
+    [key: string]: string;
+  }
+</script>
+
+<div class="flex flex-col items-center gap-16">
+  <h1 class="text-8xl font-semibold">Gallery</h1>
+  <div class="image-container">
+    {#each Object.entries(images) as [path]}
+      {@const filename = path.split("/").pop()}
+      <img src={path.replace("/static", "")} alt={getAltText(filename)} />
+    {/each}
+  </div>
+</div>

dc2f0023a216301e54c554f86e90663733540e28
LogolicusZ <adjordbra@gmail.com>
2024-12-18 20:23:49 +0100

    [c] updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import sveltePreprocess from 'svelte-preprocess';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		sveltekit({
+			preprocess: [sveltePreprocess({ typescript: true })]
+		})
+	],
 	css: {
 		preprocessorOptions: {
 			scss: {
